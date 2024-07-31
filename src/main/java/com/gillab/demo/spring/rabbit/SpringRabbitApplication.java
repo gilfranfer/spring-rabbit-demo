d84165ea15d88bb9ddbd575ad9b18eeca9fc8ac5
Franfer Gil <gil.franfer@gmail.com>
2024-07-30 20:04:06 -0600

    Initial project structure from Spring Initializr

---
+++ src/main/java/com/gillab/demo/spring/rabbit/SpringRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.gillab.demo.spring.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitApplication.class, args);
+	}
+
+}
